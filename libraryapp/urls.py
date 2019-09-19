cd8ca51948bcbff08fd34a5a28c76c095e729573
krystal gates <krystal_sabrina@yahoo.com>
2019-09-19 14:02:08 -0500

    chapter6 complete

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     url(r'^libraries$', list_libraries, name='libraries'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout'),
+    url(r'^book/form$', book_form, name='book_form'),
+    url(r'^library/form$', library_form, name='library_form'),
 ]
