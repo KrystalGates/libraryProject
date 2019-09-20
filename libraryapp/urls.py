2ad3af1e36b8347c8b5dfbcb7cf0f3bbb574e573
krystal gates <krystal_sabrina@yahoo.com>
2019-09-20 13:19:05 -0500

    chapter 7-9 links, delete, edit

--- libraryapp/urls.py
+++ libraryapp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.conf.urls import url, include
 from .views import *
+from django.urls import path
 
 app_name = "libraryapp"
 
@@ -8,10 +9,14 @@ urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^$', book_list, name='home'),
     url(r'^books$', book_list, name='books'),
+    path('books/<int:book_id>/', book_details, name='book'),
+    url(r'^books/(?P<book_id>[0-9]+)/form$', book_edit_form, name='book_edit_form'),
+    url(r'^book/form$', book_form, name='book_form'),
     url(r'^librarians$', list_librarians, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
     url(r'^libraries$', list_libraries, name='libraries'),
+    url(r'^library/form$', library_form, name='library_form'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^logout/$', logout_user, name='logout'),
-    url(r'^book/form$', book_form, name='book_form'),
-    url(r'^library/form$', library_form, name='library_form'),
 ]
